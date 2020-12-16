2a5bd4362ab4f8c3cd4bf4014e8689f9b282d136
Katarzyna Woźna <62303442+katarzynawozna@users.noreply.github.com>
2020-12-16 13:16:16 +0100

    lista 4 bez zadania 5

--- Lista4/zadanie1.py
+++ Lista4/zadanie1.py
@@ -3,6 +3,8 @@ from scipy import optimize
 def f(x):
     return (2 * (x**4) + 24 * (x**3) + 61 * (x**2) - 16 * x + 1)
 
+#intervals taken from the plot generated with WolframApha
+
 root = optimize.ridder(f, -10, -5)
 # -8.123105625616667
 
